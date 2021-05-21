6d309d19488b37959e728805a10d041476e38924
JensUweB <58864964+JensUweB@users.noreply.github.com>
2021-05-21 21:07:04 +0200

    fix: module imports

--- projects/cookie-banner/src/lib/cookie-banner.module.ts
+++ projects/cookie-banner/src/lib/cookie-banner.module.ts
@@ -1,4 +1,5 @@
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 import { CookieBannerComponent } from './cookie-banner.component';
 import { CookieService } from './cookie.service';
 import { GoogleAnalyticsService } from './google-analythics.service';
@@ -8,6 +9,7 @@ import { GoogleAnalyticsService } from './google-analythics.service';
 @NgModule({
   declarations: [CookieBannerComponent],
   imports: [
+    ReactiveFormsModule
   ],
   exports: [CookieBannerComponent],
   providers: [CookieService, GoogleAnalyticsService]
